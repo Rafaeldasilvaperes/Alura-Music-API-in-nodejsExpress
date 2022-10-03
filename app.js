f99b8d285fcea94f79292fd6226b7905e0eb3b8c
unknown <rafaeldasilvaperes@hotmail.com>
2022-10-03 19:43:11 -0300

    Adding a new Home Page for the API

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-// import { validateApiKey } from './src/helpers/keyValidator.js'
-
-// import cors from 'cors';
 
 // routes
 import { router as productsRoutes } from './src/routes/products.js';
@@ -17,15 +14,14 @@ dotenv.config();
 
 // Middleware
 app.use(bodyParser.json());
-// app.use(validateApiKey);
-// Cors allowance
-// app.use(cors());
+
 // Endpoint
 
 app.use('/products', productsRoutes);
 // Mongoose connection
 const DB_USER = process.env.DB_USER;
 const DB_PW = process.env.DB_PW;
+
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PW}@amcluster.kpqq81v.mongodb.net/?retryWrites=true&w=majority`)
 .then(() => {
   console.log('Mongoose is connected!')
@@ -34,5 +30,7 @@ mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PW}@amcluster.kpqq81v.mongodb.ne
 })
 .catch((err) => console.log(err))
 
+
 // Home Page
-app.get('/', (req, res) => res.send('Hello from the Alura Music Home Page API ♫'));
+app.get('/', (req, res) => res.sendFile('index.html', {root: './'}));
+
