873dc4e74719139a33f2365dde585277f5f04bf9
unknown <rafaeldasilvaperes@hotmail.com>
2022-10-05 19:46:02 -0300

    Trying to fix cors config string to avoid cors problem in the client application

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
   // cors
 app.use((req, res, next) =>{
   res.header("Access-Control-Allow-Origin","*")
-  res.header("Access-Control-Allow-Methods", 'GET,PATCH,POST,DELETE')
+  res.header("Access-Control-Allow-Methods", "GET,PATCH,POST,DELETE")
   app.use(cors());
   next()
 })
