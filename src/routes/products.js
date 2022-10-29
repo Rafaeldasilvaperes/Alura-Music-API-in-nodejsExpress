fa1b07fdc47343fad8ac81343112aededb780ab3
unknown <rafaeldasilvaperes@hotmail.com>
2022-10-29 17:02:11 -0300

    Deleting Multer from application, sticking with base64 strings for now

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,8 +1,6 @@
 import express from 'express';
 // routes imports
 import { createProduct, getAllProducts, getSingleProduct, deleteProduct, editProduct } from '../controllers/products-controller.js';
-// parse image
-import { upload } from '../middlewares/multer.js'
 // middleware for api_key validation
 import { validateApiKey } from '../middlewares/keyValidator.js'
 
@@ -11,7 +9,7 @@ export const router = express.Router();
 // GET
 router.get('/', validateApiKey, getAllProducts);
 // POST
-router.post('/', validateApiKey, upload.single('images') ,createProduct);
+router.post('/', validateApiKey, createProduct);
 // GET SINGLE PRODUCT
 router.get('/:id', validateApiKey, getSingleProduct);
 // DELETE SINGLE PRODUCT
