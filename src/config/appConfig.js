bfc7ba47ee139087f28cc34dd6987ed579dde7a2
Rafael Peres <rafaeldasilvaperes@hotmail.com>
2022-12-17 17:41:18 -0300

    Adding ACAO for options methods too

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -37,6 +37,7 @@ app.use((req, res, next) =>{
   );
 
   if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
     return res.status(200).send({})
   }
